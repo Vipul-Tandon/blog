9180ccaf0769dfa5504e01dada0950709217c325
Vipul Tandon <vipul.tandon@metafic.co>
2024-02-21 14:41:53 +0530

    added input validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,54 @@ class User < ApplicationRecord
     # Rails built-in method that provides password encryption using bcrypt.
     has_secure_password
     
-    # mount_uploader: :avatar, AvatarUploader
+    validates :name, length: { in: 2..50 }
     validates :email, presence: true, uniqueness: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :username, presence: true, uniqueness: true
+    
     validates :password,
-        length: { minimum: 6 },
+        presence: true,
+        length: { minimum: 8 },
         if: -> { new_record? || !password.nil? }
+
+    validate :password_format
+
+
+    private
+        def password_format
+            if password.present?
+                is_uppercase = false
+                is_lowercase = false
+                is_number = false
+                is_special = false
+                
+                password.each_char do |char|
+                    if ('A'..'Z').include?(char)
+                        is_uppercase = true
+                    elsif ('a'..'z').include?(char)
+                        is_lowercase = true
+                    elsif ('0'..'9').include?(char)
+                        is_number = true
+                    else
+                        is_special = true
+                    end
+                end
+
+                unless is_uppercase
+                    errors.add :password, "Must contain at least one uppercase letter"
+                end
+            
+                unless is_lowercase
+                    errors.add :password, "Must contain at least one lowercase letter"
+                end
+        
+                unless is_number
+                    errors.add :password, "Must contain at least one digit"
+                end
+        
+                unless is_special
+                    errors.add :password, "Must contain at least one special character"
+                end
+            end
+        end
 end
