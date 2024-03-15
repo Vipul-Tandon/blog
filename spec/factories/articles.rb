b4a5ff883d6cbd841605c9ad946d63ae405213fa
Vipul Tandon <vipul.tandon@metafic.co>
2024-03-15 14:38:58 +0530

    added comment association in articles factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
       body { Faker::Lorem.sentence}
       status { "public" }
       association :user, factory: :user
+
+
+      after(:create) do |article|
+        create_list(:comment, 3, article: article)
+      end
     end
 end
