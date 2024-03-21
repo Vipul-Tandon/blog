710e24bea6ccb112dd6b8a4f60162f07e20acbc7
mystificator <vipultandon.business@gmail.com>
2024-03-21 08:22:37 +0530

    Refactored blocked_users_spec to use hooks for sending auth token with every request

--- spec/requests/blocked_users_spec.rb
+++ spec/requests/blocked_users_spec.rb
@@ -5,10 +5,14 @@ RSpec.describe "BlockedUsers", type: :request do
   let(:other_user) { create(:user) }
   let(:token) { JsonWebToken.encode(user_id: user.id) }
 
+  before do
+    request.headers['Authorization'] = "Bearer #{token}"
+  end
+
   describe "GET /blocked_users" do
     it "returns a list of blocked users" do
       blocked_users = FactoryBot.create_list(:blocked_user, 5, user: user)
-      get '/blocked_users', headers: { 'Authorization': "Bearer #{token}" }
+      get '/blocked_users'
       
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body)).to be_an_instance_of(Array)
@@ -21,7 +25,7 @@ RSpec.describe "BlockedUsers", type: :request do
     context "when the user is not already blocked" do
       
       it "blocks the user successfully" do
-        post "/blocked_users/#{other_user.id}", headers: { 'Authorization': "Bearer #{token}" }
+        post "/blocked_users/#{other_user.id}"
         
         expect(response).to have_http_status(:ok)
         expect(JSON.parse(response.body)["message"]).to eq("User blocked successfully")
@@ -32,7 +36,7 @@ RSpec.describe "BlockedUsers", type: :request do
       
       it "returns unprocessable entity status" do
         user.blocked_users.create(blocked_user_id: other_user.id)
-        post "/blocked_users/#{other_user.id}", headers: { 'Authorization': "Bearer #{token}" }
+        post "/blocked_users/#{other_user.id}"
         
         expect(response).to have_http_status(:unprocessable_entity)
         expect(JSON.parse(response.body)["error"]).to eq("User is already blocked")
@@ -46,7 +50,7 @@ RSpec.describe "BlockedUsers", type: :request do
       
       it "unblocks the user successfully" do
         create(:blocked_user, user: user, blocked_user: other_user)
-        delete "/blocked_users/#{other_user.id}", headers: { 'Authorization': "Bearer #{token}" }
+        delete "/blocked_users/#{other_user.id}"
         
         expect(response).to have_http_status(:no_content)
       end
@@ -54,7 +58,7 @@ RSpec.describe "BlockedUsers", type: :request do
 
     context "when the user is not blocked" do
       it "returns not found status" do
-        delete "/blocked_users/#{other_user.id}", headers: { 'Authorization': "Bearer #{token}" }
+        delete "/blocked_users/#{other_user.id}"
         
         expect(response).to have_http_status(:not_found)
         expect(JSON.parse(response.body)["error"]).to eq("User is not blocked")
