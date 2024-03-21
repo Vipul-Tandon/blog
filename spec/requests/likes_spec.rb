1c2d0e822e0939fd5c9fe7ff71565400a9df8cf0
mystificator <vipultandon.business@gmail.com>
2024-03-21 16:43:48 +0530

    Refactored likes controller specs to define the headers at top

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -6,12 +6,13 @@ RSpec.describe "Likes", type: :request do
   let(:article) { create(:article) }
   let(:comment) { create(:comment, article: article) }
   let(:like) { create(:like, user: user) }
+  let(:headers) { {'Authorization': "Bearer #{token}"} }
 
 
   describe "POST /articles/:article_id/likes" do
     context "with valid params" do
       it "should return created response" do
-        post "/articles/#{article.id}/likes", headers: { 'Authorization': "Bearer #{token}" }
+        post "/articles/#{article.id}/likes", headers: headers
         
         expect(response).to have_http_status(:created)
         expect(JSON.parse(response.body)).not_to be_empty
@@ -32,7 +33,7 @@ RSpec.describe "Likes", type: :request do
   describe "POST /comments/:comment_id/likes" do
     context "with valid params" do
       it "should return created response" do
-        post "/comments/#{comment.id}/likes", headers: { 'Authorization': "Bearer #{token}" }
+        post "/comments/#{comment.id}/likes", headers: headers
         
         expect(response).to have_http_status(:created)
         expect(JSON.parse(response.body)).not_to be_empty
@@ -53,7 +54,7 @@ RSpec.describe "Likes", type: :request do
   describe "GET /articles/:article_id/likes" do
     context "with valid params" do
       it "should return created response" do
-        get "/articles/#{article.id}/likes", headers: { 'Authorization': "Bearer #{token}" }
+        get "/articles/#{article.id}/likes", headers: headers
         
         expect(response).to have_http_status(:ok)
         expect(JSON.parse(response.body)).to be_an_instance_of(Array)
@@ -62,7 +63,7 @@ RSpec.describe "Likes", type: :request do
     
     context "with invalid params" do
       it "should return error message" do
-        get "/articles/123jahhbdkj/likes", headers: { 'Authorization': "Bearer #{token}" }
+        get "/articles/123jahhbdkj/likes", headers: headers
 
         expect(response).to have_http_status(:not_found)
         expect(JSON.parse(response.body)["errors"]).to include("Article not found")
@@ -74,7 +75,7 @@ RSpec.describe "Likes", type: :request do
   describe "GET /comments/:comment_id/likes" do
     context "with valid params" do
       it "should return created response" do
-        get "/comments/#{comment.id}/likes", headers: { 'Authorization': "Bearer #{token}" }
+        get "/comments/#{comment.id}/likes", headers: headers
         
         expect(response).to have_http_status(:ok)
         expect(JSON.parse(response.body)).to be_an_instance_of(Array)
@@ -83,7 +84,7 @@ RSpec.describe "Likes", type: :request do
     
     context "with invalid params" do
       it "should return error message" do
-        get "/comments/12ekajsdnk/likes", headers: { 'Authorization': "Bearer #{token}" }
+        get "/comments/12ekajsdnk/likes", headers: headers
         
         expect(response).to have_http_status(:not_found)
         expect(JSON.parse(response.body)["errors"]).to include("Comment not found")
@@ -95,7 +96,7 @@ RSpec.describe "Likes", type: :request do
   describe "DELETE /likes/:id" do     
     context "when the like exists" do
       it "should delete the like" do
-        delete "/likes/#{like.id}", headers: { 'Authorization': "Bearer #{token}" }
+        delete "/likes/#{like.id}", headers: headers
         
         expect(response).to have_http_status(:no_content)
       end
@@ -103,7 +104,7 @@ RSpec.describe "Likes", type: :request do
     
     context "when the like does not exist" do
       it "should return a not found status" do
-        delete "/likes/1243hbjda", headers: { 'Authorization': "Bearer #{token}" }
+        delete "/likes/1243hbjda", headers: headers
         
         expect(response).to have_http_status(:not_found)
         expect(JSON.parse(response.body)["error"]).to eq("Like not found or you don't have permission to delete it")
