c6910581abafc4f2166062e4ea20f59eedda98e8
mystificator <vipultandon.business@gmail.com>
2024-03-22 19:55:25 +0530

    Refactored password reset controller to use otp instead of rails cache

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,21 +1,28 @@
 class PasswordResetsController < ApplicationController
   before_action :set_user 
+  after_action :reset_otp, only: :update
 
   # Forgot password
   def create
     otp = generate_otp
-    Rails.cache.write("password_reset_#{@user.id}", otp, expires_in: 4.minutes)
-    UserMailer.password_reset(@user, otp).deliver_now
-    render json: { message: 'OTP sent successfully', otp: otp }, status: :ok
+    # Rails.cache.write("password_reset_#{@user.id}", otp, expires_in: 4.minutes)
+    @user.otp = otp
+    if @user.save
+      UserMailer.password_reset(@user, otp).deliver_now
+      render json: { message: 'OTP sent successfully', otp: otp }, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages },
+                status: :unprocessable_entity
+    end
   end
 
   
   # Reset password
   def update
-    cached_otp = Rails.cache.read("password_reset_#{@user.id}")
-    if cached_otp.present? && params[:otp] == cached_otp
+    otp = @user.otp
+    if otp != nil && params[:otp] == otp
       if @user.update(password: params[:password], password_confirmation: params[:password_confirmation])
-        Rails.cache.delete("password_reset_#{@user.id}")
+        # Rails.cache.delete("password_reset_#{@user.id}")
         render json: { message: 'Password updated successfully' }, status: :ok
       else
         render json: { errors: @user.errors.full_messages },
@@ -37,5 +44,10 @@ class PasswordResetsController < ApplicationController
 
     def generate_otp
       SecureRandom.random_number(100000..999999)
+    end
+
+    def reset_otp
+      @user.otp = nil
+      @user.save
     end
 end
